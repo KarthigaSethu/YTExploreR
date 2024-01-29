6aeb4a8b298171e797c9f65dedfcd7b5627fd408
Andrew Sarracini <andrewsarracini1@gmail.com>
2024-01-28 21:55:50 -0800

    Added documentation, altered referencing

--- R/YTExploreR_get_uploads.R
+++ R/YTExploreR_get_uploads.R
@@ -1,19 +1,24 @@
-
+library(httr)
+library(dplyr)
+library(jsonlite)
 
 #' get_upload_activity
+#' Input a YouTube channel id and retrieves activities that are carried out by that channel.
+#' This includes: uploading videos, creating playlists (of pre-existing videos), likes activity, social activity and more.
 #'
-#' @param chan_id
 #'
-#' @return
-#' @export
+#' @param chan_id: Unique identifier string associated with a single YouTube channel.
 #'
-#' @examples
+#' @return sorted dataframe with columns associated with channel activity
+#'
+#' @examples get_uploads('UCyPYQTT20IgzVw92LDvtClw')
+
 get_uploads <- function(chan_id) {
 
   api_key <- Sys.getenv("YOUTUBE_API")
 
   params <- list(
-    part = 'snippet',
+    part = "snippet,contentDetails",
     channelId = chan_id,
     maxResults = 50,
     key = api_key
@@ -26,21 +31,20 @@ get_uploads <- function(chan_id) {
     stop('Request was not successful!')
   }
 
-  response_data <- content(r1, "parsed")
+  # response_data <- content(r1, "parsed")
+  response_data <- fromJSON(content(r1, "text"))
+  print(response_data$items[1])
 
-  uploads_df <- map_df(response_data$items, ~ data.frame(
-    publishedAt = .x$snippet$publishedAt,
-    channelId = .x$snippet$channelId,
-    title = .x$snippet$title,
-    description = .x$snippet$description,
-    channelTitle = .x$snippet$channelTitle,
-    type = .x$snippet$type
 
-  ))
+  publishedAt <- response_data$items$snippet$publishedAt
+  title <- response_data$items$snippet$title
+  type <- response_data$items$snippet$type
+  videoId <- response_data$items$contentDetails$upload$videoId
+
+  uploads_df <- data.frame(publishedAt, title, type, videoId)
 
   return(uploads_df)
 }
 
-result_df <- get_uploads('UCqFzWxSCi39LnW1JKFR3efg')
-
+result_df <- get_uploads('UCsXVk37bltHxD1rDPwtNM8Q')
 result_df
